b686b3cfcc9227375d435e6de45404c1a90dbe3a
UCLA-Adam <105705018+UCLA-Adam@users.noreply.github.com>
2024-03-08 15:33:28 -0800

    added pokemon

--- Easter Egg Code/main.py
+++ Easter Egg Code/main.py
@@ -15,12 +15,12 @@ display = adafruit_ssd1305.SSD1305_I2C(128, 32, i2c, addr=0x3C, reset=oled_reset
 
   
 # read the image file 
-img = cv2.imread('pikachu_test.jpg', 2) 
+img = cv2.imread('pokemon.png', 2) 
 
 # Resize the image adding black boarders on the outside
 # Screen resolution 
-IMG_COL = 32
-IMG_ROW = 128
+IMG_COL = 32 # length of columns 
+IMG_ROW = 128 # length of rows
 # Resize to fit resolution
 border_v = 0
 border_h = 0
